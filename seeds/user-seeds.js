1eee28291ee9edf0688264d964295f559296dddc
Abewd <116496332+abewd@users.noreply.github.com>
2023-03-14 13:08:13 +1100

    testing

---
+++ seeds/user-seeds.js
@@ -0,0 +1,21 @@
+const userData = [
+  {
+    username: "John",
+    email: "John@example.com",
+    password: "wheiothwaoi",
+  },
+  {
+    username: "Pam",
+    email: "pam@example.com",
+    password: "zerstorer",
+  },
+  {
+    username: "Mary",
+    email: "mary@example.com",
+    password: "hjgudufo",
+  },
+];
+
+const seedUsers = () => User.bulkCreate(userData, { individualHooks: true });
+
+module.exports = seedUsers;
